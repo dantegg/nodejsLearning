780ca057362ca461237509233e39d89322c10886
dantegg <luroufan327@hotmail.com>
2016-05-25 01:19:19 +0800

    new changes and files

--- 2.3.js
+++ 2.3.js
@@ -1,13 +1,11 @@
-//例2-1
-
 var net = require('net');
 var chatServer = net.createServer();
 
-
 chatServer.on('connection',function(client){
-  client.write('Hi \n');
-  client.write('Bye!\n');
-  
+  client.write('hi\n');
+  client.on('data',function(data){
+    console.log(data);
+  })
 })
 
 chatServer.listen(9000)
