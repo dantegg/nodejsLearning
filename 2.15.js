b3ce14d74758be12a0985cc3ed5bf5e42b34db0c
dantegg <luroufan327@hotmail.com>
2016-06-02 18:29:40 +0800

    add new files

--- 2.15.js
+++ 2.15.js
@@ -6,9 +6,9 @@ app.listen(8000);
 
 var tweets = [];
 
-app.get('/',function(req,res){
-  res.send('Welcome to Node Twitter');
-})
+// app.get('/',function(req,res){
+//   res.send('Welcome to Node Twitter');
+// })
 
 app.post('/send',[bodyParser.json(),bodyParser.urlencoded({ extended: true })],function(req,res){
   if(req.body && req.body.tweet){
@@ -16,6 +16,20 @@ app.post('/send',[bodyParser.json(),bodyParser.urlencoded({ extended: true })],f
   }else{
     res.send({status:'nok',message:'No tweet received'})
   }
+})
+
+app.get('/',function(req,res){
+  var title='Chirpie',
+  header="welcome to Chirpie"
+
+  res.render('index',{
+    locals:{
+      'title':title,
+      'header':header,
+      'tweets': tweets,
+      stylesheets:{'public/style.css'}
+    }
+  })
 })
 
 app.get('/tweets',function(req,res){
